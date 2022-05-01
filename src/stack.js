8ec4e89ed9336fc2cfc4890d6cdc15a86c873e73
KristiBo <kristina.boroh@gmail.com>
2022-05-02 01:30:19 +0300

    feat: add solution 2 tasks

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+    this.index = 0;
+  }
+  
+  push(element) {
+    this.stack[this.index] = element;
+    this.index += 1;
+    return this.index - 1;
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  pop() {    
+    let removeElement = this.stack[this.index - 1];
+    this.index -= 1;
+    return removeElement;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.index - 1];
   }
 }
 
